a5bac3ffee5e050c11f072381d3d89c1a9695b6a
zebra174 <amrhussam97@gmail.com>
2024-05-09 18:51:12 -0400

    static code analysis

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
-# Imports
+""" Imports"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-# Initiate the app
+
 app = Flask("Emotion Detection")
 
+"""Setting Routing"""
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """Detecting Emotion Functionalities"""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     anger = response['anger']
@@ -17,16 +19,16 @@ def sent_analyzer():
     dominant_emotion = response['dominant_emotion']
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        ans = str({"anger":anger, "disgust":disgust, "fear":fear, "joy":joy, 
-        "sadness":sadness, "dominant_emotion":dominant_emotion})
-        return "For the given statement, the system response is  " + ans
+    ans = str({"anger":anger, "disgust":disgust, "fear":fear, "joy":joy,
+    "sadness":sadness, "dominant_emotion":dominant_emotion})
+    return "For the given statement, the system response is  " + ans
 
-# Rendering Home Page
+
 @app.route("/")
 def render_index_page():
+    """Rendering Home Page"""
     return render_template('index.html')
 
-# Running application on port 5000
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
